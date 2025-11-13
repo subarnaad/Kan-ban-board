889001c3fcca07a81485c0dd043b0484322144c1
subarnaad <133129998+subarnaad@users.noreply.github.com>
2025-11-13 22:19:53 +0545

    final commit

--- kanban_project/settings.py
+++ kanban_project/settings.py
@@ -240,7 +240,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # CORS
 CORS_ALLOWED_ORIGINS = config(
     "CORS_ALLOWED_ORIGINS",
-    default="http://localhost:3000,https://kanbanfrontends.vercel.app"
+    default=""
 ).split(",")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
